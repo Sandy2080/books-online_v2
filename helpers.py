880e2c505f4a88894000e565af3be5beeff3b292
Sandy Ludosky <sludosky@xebia.fr>
2023-01-04 16:28:42 +0100

    phase 3 : get all categories and all books

--- helpers.py
+++ helpers.py
@@ -1,17 +1,18 @@
 import functions
+import os
 
 def page_number(soup):
     if soup.find('ul', {'class': 'pager'}):
         pages = soup.find('li', {'class': 'current'}).text.replace("\n", "").split(" ")
         pages = list(filter(lambda p: p != "", pages))
-    return int(pages[-1])
+        return int(pages[-1])
+    else: return 1
 
 def get_pages(url, count):
     urls = []
     index = 1
     while index <= count:
         _url = url.replace("index.html", "page-" + str(index) + ".html")
-        print(_url)
         urls.append(_url)
         index +=1
     return urls
@@ -20,9 +21,16 @@ def get_all_products(url, soup):
     all_products = []
     pages_count = page_number(soup)
     all_pages = get_pages(url, pages_count)
-    print(all_pages)
     for page in all_pages:
         page_content = functions.request(page)
         for article in page_content.find_all('article', {'class': 'product_pod'}):
             all_products.append(article)
     return all_products
+
+def create_directory(path, name):
+    dir_category_name = name.split(" ")
+    dir_category_name = "_".join(dir_category_name).lower()
+    path = path+dir_category_name
+    # if not os.path.isdir(path):
+    os.mkdir(path)    
+    return dir_category_name
